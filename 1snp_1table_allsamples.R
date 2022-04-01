7c7ae7c9a4fc4c799241c6d1ceb7cb93d2c9b3b2
Hufsah-Ashraf <63003127+Hufsah-Ashraf@users.noreply.github.com>
2022-04-01 09:08:28 +0200

    Update 1snp_1table_allsamples.R

--- 1snp_1table_allsamples.R
+++ 1snp_1table_allsamples.R
@@ -1,3 +1,4 @@
+#Hufsah Ashraf
 #makes one table 'one_table_per_snp.txt' with all haplotype configuration counts for each snp per inversion across all samples for all inversions
 library(data.table)
 library(dplyr)
@@ -27,8 +28,6 @@ whole$ref_fwd_sum<- rowSums(whole[grep('ref_fwd',names(whole))])
 whole$ref_inv_sum<- rowSums(whole[grep('ref_inv',names(whole))])
 whole$alt_fwd_sum<- rowSums(whole[grep('alt_fwd',names(whole))])
 whole$alt_inv_sum<- rowSums(whole[grep('alt_inv',names(whole))])  
-#if whole is already written
-#whole<-data.frame(fread('whole.txt'))
 one_table_per_snp<-data.frame(whole$chrom, whole$inv_start, whole$inv_end, whole$inv_ID, whole$snp_pos, whole$ref_allele, whole$alt_allele, whole$snp_AF,
                               whole$ref_fwd_sum, whole$ref_inv_sum, whole$alt_fwd_sum, whole$alt_inv_sum)
 colnames(one_table_per_snp)<-c('chrom','inv_start', 'inv_end','inv_ID','snp_pos','ref_allele','alt_allele','snp_AF','reads_ref_fwd','reads_ref_inv',
